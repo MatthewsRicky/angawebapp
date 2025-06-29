0bbafcbbf00d95d1991931158f3b9cd86772919e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-29 18:43:04 +0300

    boiler-plate input searchbar

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Searchbar() {
-  return <div>Searchbar</div>;
+  return (
+    <div>
+      <div className="flex flex-row w-10/12">
+        <input
+          type="text"
+          placeholder="Enter location"
+          className="border-gray-600/40 text-md px-3 py-1 border-2 rounded-lg text-sm text-gray-500 font-regular"
+        />
+      </div>
+    </div>
+  );
 }
