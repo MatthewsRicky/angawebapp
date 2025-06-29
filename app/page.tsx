beba6d751ced4252b2436c52b5d3be8ffee57ae5
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-29 18:39:31 +0300

    reorganizing components

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import { useSearchParams } from "next/navigation";
 import React from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function page() {
   const params = useSearchParams();
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-2xl bg-gradient-to-b from-blue-50 to-blue-100">
-      <Header />
+    <div className="h-screen flex flex-col items-center justify-center text-2xl bg-gradient-to-b from-blue-50 to-blue-100 mx-auto">
+      <Hero />
     </div>
   );
 }
