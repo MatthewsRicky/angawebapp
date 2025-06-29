ab7c8d9449b0dfc23b2e1f6a4d76b326c74464f8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-29 18:42:21 +0300

    Basic styling

---
+++ app/components/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Searchbar from "./Searchbar";
+import CurrentWeatherCard from "./CurrentWeatherCard";
+import WeeklyWeatherCard from "./WeeklyWeatherCard";
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center mx-auto w-[80%]">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col md:flex-row gap-4">
+          <span className="">
+            <CurrentWeatherCard />
+          </span>
+          <span>
+            <WeeklyWeatherCard />
+          </span>
+        </div>
+
+        <h1 className="flex w-[80%] text-center">
+          Search for the weather forecast in your Location
+        </h1>
+        <Searchbar />
+      </div>
+    </div>
+  );
+}
