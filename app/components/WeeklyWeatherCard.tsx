2c4f3199c8e104cc1fdb105dfda9b569f1208b9d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-30 22:50:13 +0300

    Weekly weather card build and rendering through boiler plate data mapping

--- app/components/WeeklyWeatherCard.tsx
+++ app/components/WeeklyWeatherCard.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { BiCloudDrizzle, BiMoon, BiSun } from "react-icons/bi";
 import { MdArrowRightAlt } from "react-icons/md";
+import { weatherCardData } from "../lib/data";
 
 export default function WeeklyWeatherCard() {
   return (
-    <main className="flex items-center justify-center">
+    <main className="flex items-center justify-center w-full">
       <div className="grid grid-cols-7 gap-4 p-3 rounded-xl items-center border-2 border-blue-300/40 justify-center shadowxl">
-        <div className="flex h-full flex-col items-center justify-center bg-element">
-          <div className="flex flex-col gap-2 items-center">
-            <h1 className="row-span-1">Sun</h1>
-            <h3 className="text-base">06 / 29</h3>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <BiCloudDrizzle className="text-3xl text-center mt-3" />
-            <h3 className="text-base line-clamp-2 text-center">
-              Light drizzle
-            </h3>
-          </div>
+        {weatherCardData.map((index) => (
+          <div className="flex h-90 flex-col items-center justify-between rounded-md bg-element">
+            <div className="flex flex-col gap-2 items-center mt-3">
+              <h1 className="row-span-1">{index.day}</h1>
+              <h3 className="text-base">{index.date}</h3>
+            </div>
+            <div className="flex flex-col items-center justify-between gap-3">
+              <BiCloudDrizzle className="text-3xl text-center mt-3" />
+              <h3 className="text-xs line-clamp-2 text-center">
+                {index.title}
+              </h3>
+            </div>
 
-          <span className="flex gap-1 items-center justify-center mt-2 text-base">
-            <BiSun />
-            <h1>
-              25<sup>0</sup>c
-            </h1>
-          </span>
-          <span className="flex gap-1 items-center justify-center mt-2 text-base">
-            <BiMoon />
-            <h1>
-              15<sup>0</sup>c
-            </h1>
-          </span>
-          <div className="flex flex-col">
-            <MdArrowRightAlt className="text-base my-3" />
-            <span>
-              <h3 className="text-base">17km/h</h3>
+            <span className="flex gap-1 items-center justify-center mt-2 text-base">
+              <BiSun />
+              <h1>
+                {index.high}<sup>0</sup>c
+              </h1>
             </span>
+            <span className="flex gap-1 items-center justify-center mt-2 text-base">
+              <BiMoon />
+              <h1>
+                {index.low}<sup>0</sup>c
+              </h1>
+            </span>
+            <div className="flex flex-col items-center justify-between mb-3">
+              <MdArrowRightAlt className="text-base" />
+              <span>
+                <h3 className="text-base">{index.windspeed} km/h</h3>
+              </span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </main>
   );
