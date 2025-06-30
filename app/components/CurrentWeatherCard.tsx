1f3636d10d30d5d309a8ecc9312748a77bc41e56
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-06-30 22:49:03 +0300

    Building out the Current card design

--- app/components/CurrentWeatherCard.tsx
+++ app/components/CurrentWeatherCard.tsx
@@ -7,23 +7,24 @@ import {
   BsFillMoonFill,
   BsFillSunFill,
 } from "react-icons/bs";
+import { weatherCardData} from "../lib/data";
 
 export default function CurrentWeatherCard() {
   return (
-    <main className="flex items-center justify-center">
-      <div className="flex flex-col items-center justify-between py-2 px-3 min-w-w-fit border-2 rounded-2xl bg-gradient from-blue-50/70 to-blue-100/90 border-blue-500/20 shadowxl w-full">
+    <main className="flex items-center justify-center w-full">
+      <div className="flex flex-col items-center justify-between py-2 px-3 min-w-w-fit border-2 rounded-2xl bg-gradient from-blue-50/70 to-blue-100/90 border-blue-500/20 shadowxl">
         <h1 className="text-base font-redular text-gray-600/90 shadowsm py-1 px-2 rounded-sm mt-1 bg-element">
-          Today
+          Sunday 29 | 06
         </h1>
 
-        <div className="flex w-full text-gray-700/80 items-center justify-between shadowsm p-3 mt-4 rounded-xl bg-element">
-          <h2 className="text-3xl font-medium my-2">Rainy</h2>
-          <span className="text-7xl p-4 w-1/2">
+        <div className="flex w-full text-gray-700/80 items-center justify-around shadowsm p-3 mt-4 rounded-xl bg-element">
+          <span className="flex flex-col text-7xl p-2 items-center justify-between">
             <BsCloudRainHeavy />
+            <h2 className="text-3xl font-medium my-2">Rainy</h2>
           </span>
 
           <div className="flex items-baseline">
-            <p className="font-medium text-8xl">
+            <p className="font-medium text-6xl">
               25
               <sup>o</sup>
             </p>
@@ -32,12 +33,12 @@ export default function CurrentWeatherCard() {
         </div>
 
         <div className="w-full flex flex-col items-start mb-3 py-2 px-1 justify-between text-gray-600">
-          <div className="flex items-center justify-between w-full">
+          <div className="flex items-center justify-around w-full">
             <h3>More details</h3>
             <BiSolidRightArrow />
           </div>
         </div>
-        <div className="flex px-2 items-center w-full justify-around gap-6 text-base bg-element rounded-lg shadowsm mb-1">
+        <div className="flex px-2 items-center w-full justify-around text-base bg-element rounded-lg shadowsm mb-1">
           <span className="flex items-center justify-center gap-3 my-1 p-2">
             <BsFillSunFill className="text-amber-300 shadowsm rounded-full" />
             <h3>High</h3>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Searchbar from './Searchbar'
+import { BiCloud, BiCloudDrizzle, BiCloudLightning, BiCloudLightRain, BiCloudRain, BiCloudSnow, BiSun, BiWind } from 'react-icons/bi'
 
 export default function Header() {
   return (
     <div>
       <Searchbar />
+      <BiCloud />
+      <BiCloudDrizzle />
+      <BiCloudSnow />
+      <BiSun />
+      <BiCloudRain />
+      <BiCloudLightning />
+      <BiWind />
     </div>
   )
 }
