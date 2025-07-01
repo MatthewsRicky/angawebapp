2e1ea42d521353fa8bbf96c982babeebdec39844
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-01 23:10:30 +0300

    editing the api helper function for daily and weekly updates. still using fixed lat nd lon values

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -5,14 +5,21 @@ export type WeatherResponse = {
     weathercode: number;
     time: string;
   };
+  daily: {
+    time: string[]; // e.g., ["2024-06-27", ...]
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+    wind_speed_10m_max: number[];
+    weathercode: number[];
+  };
 };
 
-export async function fetchWeatherByCoords(
+export async function fetchFullWeather(
   lat: number,
   lon: number
 ): Promise<WeatherResponse> {
   const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m,weathercode&timezone=auto`
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,wind_speed_10m_max,weathercode&timezone=auto&forecast_days=7`
   );
 
   if (!response) {
