48eae9dfb45298972a079fe3021ba8a6dbf77af8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-04 21:35:10 +0300

    styling edits

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ import WeeklyWeatherCard from "./WeeklyWeatherCard";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center mx-auto w-full">
+    <div className="flex flex-col lg:flex-row items-center h-screen justify-around mx-auto w-full">
       <div className="flex flex-col gap-8 w-full">
         <div className="flex flex-col md:flex-row gap-4">
           <CurrentWeatherCard className="h-[100%]" children />
