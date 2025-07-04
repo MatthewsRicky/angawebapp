b8816cb262dfdaa414638fc9410bebdd7418d493
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-04 19:46:20 +0300

    Weekly weeather card complete

--- app/components/WeeklyWeatherCard.tsx
+++ app/components/WeeklyWeatherCard.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { BiCloudDrizzle, BiMoon, BiSun } from "react-icons/bi";
-import { MdArrowRightAlt } from "react-icons/md";
+import { MdArrowRight, MdArrowRightAlt } from "react-icons/md";
 import { weatherCardData } from "../lib/data";
 import { fetchFullWeather } from "../lib/api";
+import { BsFillSunFill } from "react-icons/bs";
 
 export default function WeeklyWeatherCard() {
   const [weather, setWeather] = useState<any>(null);
@@ -32,14 +33,25 @@ export default function WeeklyWeatherCard() {
   const daily = weather.daily;
 
   return (
-    <main className="flex items-center justify-center w-full h-full">
-      <div className="grid grid-cols-7 gap-4 p-3 rounded-t-md rounded-b-2xl items-center border-2 border-blue-300/40 justify-between shadowxl h-full">
+    <main className="flex flex-col items-center justify-center w-full mx-auto xl:min-w-full min-h-full">
+      <div
+        className="flex w-full px-3 mb-1 justify-center  rounded-t-md rounded-b-2xl items-center border-2 border-blue-300/40 shadowsm"
+        key={current.time}
+      >
+        <p className="font-extrabold uppercase text-gray-700/70 text-xs">
+          {new Date(current.time).toLocaleDateString(undefined, {
+            month: "long",
+          })}
+        </p>
+      </div>
+
+      <div className="grid grid-cols-7 gap-3 pb-2 px-2 w-full rounded-t-md rounded-b-2xl items-center border-2 border-blue-300/40 justify-between shadowxl h-full">
         {daily.time.map((date: string, i: number) => (
           <div
             key={date}
-            className="flex flex-col items-center justify-around rounded-md bg-element shadowxl "
+            className="flex flex-col items-center justify-around rounded-md bg-element shadowsm "
           >
-            <p className="font-regular text-base">
+            <p className="font-semibold text-xs text-gray-700/90 p">
               {new Date(date).toLocaleDateString(undefined, {
                 weekday: "short",
                 // month: "short",
@@ -61,26 +73,29 @@ export default function WeeklyWeatherCard() {
               </h3>
             </div> */}
 
-            <span className="flex flex-col gap-1 items-center justify-center mt-2 text-base">
-              <BiSun />
+            <span className="flex flex-col gap-1 items-center justify-center mt-2 text-xs">
+              <BsFillSunFill className="text-yellow-400" />
+              <p>high</p>
               <h1>
                 {daily.temperature_2m_max[i]}
                 <sup>0</sup>c
               </h1>
             </span>
-            <span className="flex flex-col gap-1 items-center justify-center mt-2 text-base">
-              <BiMoon />
+            <span className="flex flex-col gap-1 items-center justify-center mt-2 text-xs">
+              <BiMoon className="text-yellow-400" />
               <h1>
+                <p>low</p>
                 {daily.temperature_2m_min[i]}
                 <sup>0</sup>c
               </h1>
             </span>
             <div className="flex flex-col items-center justify-between mb-3">
-              <MdArrowRightAlt className="text-base" />
+              <div className="flex items-center justify-center">
+                💨
+                <MdArrowRightAlt className="text-base" />
+              </div>
               <span>
-                <h3 className="text-base">
-                  {daily.wind_speed_10m_max[i]} km/h
-                </h3>
+                <h3 className="text-xs">{daily.wind_speed_10m_max[i]} km/h</h3>
               </span>
             </div>
           </section>
