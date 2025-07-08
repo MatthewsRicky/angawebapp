cd3dee4d51909408c4da7a3f52c1a0148a11c234
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-08 18:57:01 +0300

    Implimenting the search functionality

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { fetchCoordsByCity, fetchFullWeather } from "../lib/api";
 
 export default function Searchbar() {
+  const [weather, setWeather] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState("");
+
+  const handleSearch = async () => {
+    if (!city.trim()) return;
+    setLoading(true);
+    setError(null);
+
+    try {
+      const coords = await fetchCoordsByCity(city);
+      const data = await fetchFullWeather(coords.lat, coords.lon);
+      setWeather(data);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <div className="flex flex-row w-10/12">
         <input
           type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
           placeholder="Enter location"
-          className="border-gray-600/40 text-md px-3 py-1 border-2 rounded-lg text-sm text-gray-500 font-regular"
+          className="border-gray-600/40 text-md px-3 py-1 border-2 rounded-lg text-sm text-gray-900 font-regular bg-gray-200"
         />
+        <button
+          onClick={handleSearch}
+          className="bg-element text-white px-4 py-1 ml-2 rounded-lg text-sm font-semibold hover:bg-gray-600/60 transition-colors duration-300"
+          disabled={loading}
+        >
+          {loading ? "Searching..." : "Search"}
+        </button>
       </div>
     </div>
   );
