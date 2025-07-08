27fe5e5d75b4b004592eb8f3e54aab310ef401ba
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-08 18:56:33 +0300

    Building the header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Searchbar from './Searchbar'
-import { BiCloud, BiCloudDrizzle, BiCloudLightning, BiCloudLightRain, BiCloudRain, BiCloudSnow, BiSun, BiWind } from 'react-icons/bi'
+import React from "react";
+import Navbar from "./Navbar";
+import Searchbar from "./Searchbar";
+import {
+  BiCloud,
+  BiCloudDrizzle,
+  BiCloudLightning,
+  BiCloudLightRain,
+  BiCloudRain,
+  BiCloudSnow,
+  BiSun,
+  BiWind,
+} from "react-icons/bi";
 
 export default function Header() {
   return (
-    <div>
-      <Searchbar />
-      <BiCloud />
-      <BiCloudDrizzle />
-      <BiCloudSnow />
-      <BiSun />
-      <BiCloudRain />
-      <BiCloudLightning />
-      <BiWind />
-    </div>
-  )
+      <header className="flex items-center justify-between w-full h-20 bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-lg">
+        <div className="flex items-center justify-between w-full max-w-6xl px-4">
+          <h1 className="text-2xl font-bold">Weather App</h1>
+          <Searchbar />
+        </div>
+      </header>
+    
+  );
 }
