6a3865243f3b72bb2a4d6a290f5c7d6c5f0508af
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-13 23:43:00 +0300

    Clean footere Boiler plate

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="text-center text-gray-500 mt-8">
+        © {new Date().getFullYear()} Weather App
+      </footer>
+      <p className="text-center text-gray-500 mt-2">
+        {" "}
+        Built with Next.js and Open-Meteo API
+      </p>
+    </div>
+  );
 }
