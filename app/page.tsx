1dd2293f93e74e0f69aa4d9516412eaa1eeb0e5a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-14 20:58:59 +0300

    cleaning up the main page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-
-import { useEffect, useState } from "react";
 import Hero from "./components/Hero";
-import { fetchFullWeather } from "./lib/api";
 
 export default function Home() {
-
-
   return (
     <div className="p-4 max-w-2xl mx-auto">
       <Hero />
