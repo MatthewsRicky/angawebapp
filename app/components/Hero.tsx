ac78669d1d8b7dc0e968e30d35d1b5348666b719
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-15 02:00:21 +0300

    more layout

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,11 +8,11 @@ export default function Hero() {
   const [coords, setCoords] = useState({ lat: -4.322222, lon: 39.575001 }); // Diani default
 
   return (
-    <div className="flex flex-col lg:flex-row items-center h-screen justify-around mx-auto w-full xl:w-[80%]">
+    <div className="flex flex-col lg:flex-row items-center h-screen justify-around mx-auto mb-10 w-full xl:w-[80%]">
       <div className="flex flex-col gap-8 w-full">
         <Searchbar onSearch={setCoords} />
 
-        <div className="flex flex-col lg:flex-row gap-2 xl:gap-12">
+        <div className="flex flex-col md:flex-row gap-2 xl:gap-12">
           <CurrentWeatherCard
             className="h-[100%]"
             lat={coords.lat}
