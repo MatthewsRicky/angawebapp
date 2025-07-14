95db8b4ba58b650c7500dc540d05acb2ea3c43b7
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2025-07-14 21:01:38 +0300

    removing the search from the nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Navbar from "./Navbar";
+"use client";
+import { useState } from "react";
 import Searchbar from "./Searchbar";
 import {
   BiCloud,
@@ -13,13 +13,12 @@ import {
 } from "react-icons/bi";
 
 export default function Header() {
+  const [coords, setCoords] = useState({ lat: -4.322222, lon: 39.575001 }); // Diani default
   return (
-      <header className="flex items-center justify-between w-full h-20 bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-lg">
-        <div className="flex items-center justify-between w-full max-w-6xl px-4">
-          <h1 className="text-2xl font-bold">Weather App</h1>
-          <Searchbar />
-        </div>
-      </header>
-    
+    <header className="flex items-center justify-between w-full h-20 bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-lg">
+      <div className="flex items-center justify-between w-full max-w-6xl px-4">
+        <h1 className="text-2xl font-bold">Weather App</h1>
+      </div>
+    </header>
   );
 }
